3f9fbdfb3dca80be2bff44ce18ebc5797d4356ab
Moyukh <141808633+MoyukhRoy@users.noreply.github.com>
2025-03-14 00:39:46 +0530

    Update HousePricing_app.py

--- HousePricing_app.py
+++ HousePricing_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
