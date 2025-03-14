ff5b1db50a51dedf82d650afa54a4e8b937751f7
Moyukh <141808633+MoyukhRoy@users.noreply.github.com>
2025-03-14 10:12:01 +0530

    Update HousePricing_app.py

--- HousePricing_app.py
+++ HousePricing_app.py
@@ -10,10 +10,10 @@ st.write("Enter details below to get an estimated house price:")
 col1, col2, col3 = st.columns(3)
 with col1:
     housing_median_age = st.number_input("Housing Median Age", min_value=1, max_value=100, value=30)
-    total_rooms = st.number_input("Total Rooms", min_value=1, max_value=50000, value=5000)
+    total_rooms = st.number_input("Total Rooms", min_value=1, max_value=50000, value=12)
 with col2:
-    total_bedrooms = st.number_input("Total Bedrooms", min_value=1, max_value=10000, value=1000)
-    population = st.number_input("Population", min_value=1, max_value=50000, value=3000)
+    total_bedrooms = st.number_input("Total Bedrooms", min_value=1, max_value=10000, value=5)
+    population = st.number_input("Population", min_value=1, max_value=50000, value=14)
 with col3:
     households = st.number_input("Households", min_value=1, max_value=10000, value=1000)
     median_income = st.number_input("Median Income", min_value=0.1, max_value=20.0, value=5.0)
